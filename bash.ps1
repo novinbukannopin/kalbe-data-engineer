5651910ca6d2cb1294aeb02908267d9891924e0f
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2023-07-29 22:43:43 +0700

    task 3

--- bash.ps1
+++ bash.ps1
@@ -1,6 +1,7 @@
 param(
     [string]$path = "hdfs/",
-    [string]$name_of_directory = "data"
+    [string]$name_of_directory = "data",
+    [string]$excel = "source/dataset.xlsx"
 )
 
 # Check if the directory exists
@@ -12,15 +13,35 @@ else {
     Write-Host "$($name_of_directory) Directory Not Exists!"
     # Create the directory
     New-Item -ItemType Directory -Path $directoryPath | Out-Null
+
+    # Final Step: Create a scheduled task to run the script daily at 07:00 AM
+    $trigger = New-ScheduledTaskTrigger -Daily -At 7am
+
+    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File ""C:\Users\Novin\Downloads\vix\bash.ps1"""
+    Register-ScheduledTask -TaskName "CheckDirectoryTask" -Trigger $trigger -Action $action -User "Novin" -RunLevel Highest
+    Write-Host "Scheduled task 'CheckDirectoryTask' created to run the script daily at 07:00 AM."
 }
 
-# Final Step: Create a scheduled task to run the script daily at 07:00 AM
-$trigger = New-ScheduledTaskTrigger -Daily -At 7am
 
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File ""C:\Users\Novin\Downloads\vix\bash.ps1"""
-if (
-Register-ScheduledTask -TaskName "CheckDirectoryTask" -Trigger $trigger -Action $action -User "Novin"   -RunLevel Highest
-){
-Write-Host "Scheduled task 'CheckDirectoryTask' created to run the script daily at 07:00 AM."
+
+function Write-Log {
+    param (
+        [string]$LogPath,
+        [string]$Message
+    )
+    $LogMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $Message"
+    Add-Content -Path $LogPath -Value $LogMessage
+}
+
+if (Test-Path $excel -PathType Leaf) {
+    $targetDir = Join-Path $directoryPath $filename_excel
+    Copy-Item $excel -Destination $targetDir -Force
+    Write-Host "File '$filename_excel' copied successfully from '$source_dir' to '$directoryPath'."
+
+    $logFilePath = Join-Path $directoryPath "CopyLogFile.txt"
+    Write-Log -LogPath $logFilePath -Message "File Moved Successfully"
+}
+else {
+    Write-Host "File '$filename_excel' not found in the source directory '$source_dir'."
 }
 
---
+++ main.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+db_host = "localhost"
+db_port = "5432"
+db_name = "kalbe"
+db_user = "postgres"
+db_password = "novinganteng"
+
+try:
+    conn = psycopg2.connect(host=db_host, port=db_port, database=db_name, user=db_user, password=db_password)
+    print("Connect")
+
+    cur = conn.cursor()
+
+    emp_f_name = "Novin"
+    emp_l_name = "Ardian"
+    age = 19
+    sex = "male"
+    income = 50000
+
+    insert_query = f"INSERT INTO karyawan (first_name, last_name, age, sex, income) VALUES (%s, %s, %s, %s, %s)"
+
+    cur.execute(insert_query, (emp_f_name, emp_l_name, age, sex, income))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Data saved succesfully')
+except Exception as e:
+    print("Terjadi kesalahan:", e)
